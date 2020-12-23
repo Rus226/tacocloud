26699b86313835485e9a2ccb56c1692bec5a98a2
sairutdinov <r.sairutdinov@haulmont.com>
2020-12-23 13:12:08 +0400

    add security

---
+++ src/main/java/sia/tacocloud/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package sia.tacocloud.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new StandardPasswordEncoder("53cr3t");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .hasRole("ROLE_USER")
+                .antMatchers("/", "/**")
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
+
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("buzz")
+//                .password("infinity")
+//                .authorities("ROLE_USER")
+//                .and()
+//                .withUser("woody")
+//                .password("bullseye")
+//                .authorities("ROLE_USER");
+//        auth
+//                .jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .passwordEncoder(new StandardPasswordEncoder("53rc3t"));
+//
+//        auth
+//                .ldapAuthentication()
+//                .userSearchBase("ou=people")
+//                .userSearchFilter("(uid={0})")
+//                .groupSearchBase("ou=groups")
+//                .groupSearchFilter("member={0}")
+//                .passwordCompare();
+    }
+}
