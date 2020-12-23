c27bd3f0751deb42a3ab68bffe38cacd650b3c28
sairutdinov <r.sairutdinov@haulmont.com>
2020-12-23 10:06:09 +0400

    add field

--- src/main/java/sia/tacocloud/entity/Taco.java
+++ src/main/java/sia/tacocloud/entity/Taco.java
@@ -4,10 +4,14 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Taco {
+
+    private Long id;
+    private Date createdAt;
 
     @NotNull
     @Size(min=5, message="Name must be at least 5 characters long")
